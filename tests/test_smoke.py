6adab793f6fd327225e34cbd6e2c76508e368791
CMBorjas <118933346+CMBorjas@users.noreply.github.com>
2025-10-01 15:39:31 -0600

    test: add smoke + quick training tests; make data a package

---
+++ tests/test_smoke.py
@@ -0,0 +1,7 @@
+﻿import numpy as np
+from neuron_vz import VZNetwork
+
+def test_feedforward_runs_between_0_and_1():
+    net = VZNetwork(seed=0)
+    out = net.feedforward(np.array([0.0, 0.0]))
+    assert 0.0 <= out <= 1.0
